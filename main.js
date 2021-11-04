2a047e7c9c2e075b9762abd43254f3f7b413fe42
ValerioMik <valerio.avallone@hotmail.com>
2021-11-04 15:15:45 +0100

    Primo pseudo codice con arrey di ogg.

---
+++ main.js
@@ -0,0 +1,53 @@
+/* Ricreiamo le card del nostro team aggiungendo al layout di base fornito, il nostro javascript in cui:
+- Creiamo il nostro array di oggetti che rappresentano ciascun membro del team. (nel nostro file js)
+Ogni membro dovrà avere le informazioni necessarie per stampare la relativa card: Nome, Ruolo e Foto.
+- Prendendo come riferimento il layout di esempio presente nell’html, stampiamo tutte le card del nostro team.
+- Utilizziamo poi gli input presenti nella pagina per permettere all’utente di aggiungere nuovi membri del team. */
+
+//1.creo un arrey di oggetti che contengono le card,i testi adiacenti(nome,ruolo)
+// tramite un ciclo per gli oggetti vado ad estrapolarmi le varie informazioni
+// stampo nel html le card con relativi nomi
+// collego il form ad una variabile che mi pusha i nuovi membri nell'html
+
+let team = [
+
+    {
+        "imgTeam" : "img\wayne-barnett-founder-ceo.jpg",
+        "nomeTeam":"Wayne Barnett",
+        "ruoloTeam": "founder ceo"
+    },
+
+    {
+        "imgTeam" : "img\angela-caroll-chief-editor.jpg",
+        "nomeTeam":"Angela Carol",
+        "ruoloTeam": "chief editor"
+    },
+
+    {
+        "imgTeam" : "img\angela-lopez-social-media-manager.jpg",
+        "nomeTeam":"Angela Lopez",
+        "ruoloTeam": "media manager"
+    },
+
+    {
+        "imgTeam" : "img\barbara-ramos-graphic-designer.jpg",
+        "nomeTeam":"Barbara Ramos",
+        "ruoloTeam": "graphic designer"
+    },
+
+    {
+        "imgTeam" : "img\scott-estrada-developer.jpg",
+        "nomeTeam":"Scott Estrada",
+        "ruoloTeam": "developer "
+    },
+
+   
+
+    {
+        "imgTeam" : "img\angela-caroll-chief-editor.jpg",
+        "nomeTeam":"Angela Carol",
+        "ruoloTeam": "chief editor"
+    },
+
+    
+]
